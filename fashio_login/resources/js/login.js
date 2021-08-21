057f992b88cc39e5520063f343604c08d9090362
Shivaansh-Srivastava <72780341+Shivaansh-Srivastava@users.noreply.github.com>
2021-08-21 18:20:31 +0530

    login_page

---
+++ fashio_login/resources/js/login.js
@@ -0,0 +1,16 @@
+<script>
+    function logins() {
+        
+        var names = document.getElementById('user').value;
+        var passs = document.getElementById('password').value;
+        var result = confirm("Will redirect to a different page. Want to continue?");
+        if(result == true)
+        {
+            alert("Thanks for visiting.")
+        }
+        else
+        {
+            alert("Thanks for Visiting.")
+        }
+    &rbrace;
+</script>
